b6f734a73ab8b383b79f234c07dcb586e09a3d7e
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2020-09-14 02:28:35 -0400

    Start of bril-rs! turns bril programs into cfg and back

---
+++ bril-rs/src/lib.rs
@@ -0,0 +1,4 @@
+#![crate_type = "lib"]
+#![feature(option_unwrap_none)]
+pub mod cfg;
+pub mod program;
---
+++ bril-rs/src/program.rs
@@ -0,0 +1,202 @@
+use serde::{Deserialize, Serialize};
+use std::io::{self, Read};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Program {
+    pub functions: Vec<Function>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Function {
+    pub name: String,
+    pub args: Option<Vec<Argument>>,
+    #[serde(rename = "type")]
+    pub return_type: Option<Type>,
+    pub instrs: Vec<Code>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Argument {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub arg_type: Type,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum Code {
+    Label { label: String },
+    Instruction(Instruction),
+}
+
+impl From<Code> for String {
+    fn from(item: Code) -> Self {
+        match item {
+            Code::Label { label } => format!("Label {}", label),
+            Code::Instruction(i) => i.into(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum Instruction {
+    Constant {
+        op: ConstOps,
+        dest: String,
+        #[serde(rename = "type")]
+        const_type: Type,
+        value: Literal,
+    },
+    Value {
+        op: ValueOps,
+        dest: String,
+        #[serde(rename = "type")]
+        op_type: Type,
+        args: Option<Vec<String>>,
+        funcs: Option<Vec<String>>,
+        labels: Option<Vec<String>>,
+    },
+    Effect {
+        op: EffectOps,
+        args: Option<Vec<String>>,
+        funcs: Option<Vec<String>>,
+        labels: Option<Vec<String>>,
+    },
+}
+
+// I am going to assume that each of these has been checked on creation so I'm leaving out a bunch of asserts. These can be added if stuff looks weird
+impl From<Instruction> for String {
+    fn from(item: Instruction) -> Self {
+        match item {
+            Instruction::Constant {
+                op: ConstOps::Const,
+                dest,
+                const_type,
+                value,
+            } => format!("{} : {:?} = const {:?}", dest, const_type, value),
+            Instruction::Value {
+                ..
+                /* op,
+                dest,
+                op_type,
+                args,
+                funcs,
+                labels, */
+            } => unimplemented!(),
+            Instruction::Effect {
+                op: EffectOps::Jump,
+
+                labels,
+                ..
+            } => format!("jmp {}", labels.unwrap()[0]),
+            Instruction::Effect {
+                op: EffectOps::Branch,
+                args,
+                labels,
+                ..
+            } => {
+                let l = labels.unwrap();
+                format!("br {} {} {}", args.unwrap()[0], l[0], l[1])
+            }
+            Instruction::Effect {
+                op: EffectOps::Call,
+                args,
+                funcs,
+                ..
+            } => match args {
+                Some(a) => format!("call {} {}", funcs.unwrap()[0], a.join(" ")),
+                None => "print".to_string(),
+            }
+            Instruction::Effect {
+                op: EffectOps::Return,
+                args,
+                ..
+            } => match args {
+                Some(a) => format!("ret {}", a[0]),
+                None => "ret".to_string(),
+            },
+            Instruction::Effect {
+                op: EffectOps::Print,
+                args,
+                ..
+            } => match args {
+                Some(a) => format!("print {}", a.join(" ")),
+                None => "print".to_string(),
+            },
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ConstOps {
+    #[serde(rename = "const")]
+    Const,
+}
+
+// Todo Can I handle ops in a better way because call overlaps?
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum EffectOps {
+    #[serde(rename = "jmp")]
+    Jump,
+    #[serde(rename = "br")]
+    Branch,
+    #[serde(rename = "call")]
+    Call,
+    #[serde(rename = "ret")]
+    Return,
+    #[serde(rename = "print")]
+    Print,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ValueOps {
+    #[serde(rename = "add")]
+    Add,
+    #[serde(rename = "sub")]
+    Sub,
+    #[serde(rename = "mul")]
+    Mul,
+    #[serde(rename = "div")]
+    Div,
+    #[serde(rename = "eq")]
+    Eq,
+    #[serde(rename = "lt")]
+    Lt,
+    #[serde(rename = "gt")]
+    Gt,
+    #[serde(rename = "le")]
+    Le,
+    #[serde(rename = "ge")]
+    Ge,
+    #[serde(rename = "not")]
+    Not,
+    #[serde(rename = "and")]
+    And,
+    #[serde(rename = "or")]
+    Or,
+    #[serde(rename = "call")]
+    Call,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Type {
+    #[serde(rename = "int")]
+    Int,
+    #[serde(rename = "bool")]
+    Bool,
+    //TODO There is also some parameterized pointer type
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum Literal {
+    Int(u32),
+    Bool(bool),
+}
+
+pub fn load_program() -> Program {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    serde_json::from_str(&buffer).unwrap()
+}
