ed3706508331e5bb0d81f336efd9c92f27a1cf15
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2020-11-27 01:42:44 -0500

    A bril interpreter

---
+++ brili-rs/src/main.rs
@@ -0,0 +1,29 @@
+use bril_rs::load_program;
+use brili_rs::interp::eval_program;
+use clap::{App, Arg};
+
+fn main() {
+    let args = App::new("brili-rs")
+        .author("Patrick LaFontaine")
+        .about("Interprets bril programs")
+        .arg(Arg::with_name("profiling").short("p").takes_value(false))
+        .arg(Arg::with_name("arguments").multiple(true).allow_hyphen_values(true))
+        .get_matches();
+
+    let other_args = if args.is_present("arguments") {
+        args.values_of("arguments").unwrap().collect()
+    } else {
+        vec![]
+    };
+
+    // todo Point 3 of a well formed bril program is that the runtime type of a variable does not change within a function. I could probably go implement that
+
+    let program = load_program();
+    match eval_program(program, args.is_present("profiling"), other_args) {
+        Ok(()) => std::process::exit(0),
+        Err(s) => {
+            eprintln!("{}", s);
+            std::process::exit(2)
+        }
+    }
+}
