898f83907d45a5f84521bdb530a890fc3f77f483
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2022-02-25 14:30:18 -0500

    try using honggfuzz instead?

--- brilirs/brilirs-fuzz/src/main.rs
+++ brilirs/brilirs-fuzz/src/main.rs
@@ -1,13 +1,16 @@
 use bril_rs::Program;
 use brilirs::{basic_block, check, interp};
-use afl::fuzz;
+
+use honggfuzz::fuzz;
 
 fn main() {
-  fuzz!(|prog: Program| {
-    if let Ok(bbprog) = basic_block::BBProgram::new(prog) {
-      if let Ok(()) = check::type_check(&bbprog) {
-        if let Ok(()) = interp::execute_main(&bbprog, std::io::stdout(), &vec![String::new()], true) {}
+  loop {
+    fuzz!(|prog: Program| {
+      if let Ok(bbprog) = basic_block::BBProgram::new(prog) {
+        if let Ok(()) = check::type_check(&bbprog) {
+          if let Ok(()) = interp::execute_main(&bbprog, std::io::stdout(), &vec![], true) {}
+        }
       }
-    }
-  });
+    });
+  }
 }
