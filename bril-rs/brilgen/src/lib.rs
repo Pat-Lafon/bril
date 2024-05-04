94e03e6264daf5d64b4329b82920ebcb017b93b6
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2024-05-03 20:02:36 -0700

    wip

--- bril-rs/brilgen/src/lib.rs
+++ bril-rs/brilgen/src/lib.rs
@@ -1,15 +1,17 @@
 use core::num;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::num::NonZeroU32;
+use std::ops::Range;
 
-use bril_rs::{self, Argument, ConstOps, Function, Instruction, Literal, Program, Type};
-use rand::{random, seq::SliceRandom, thread_rng};
+use bril_rs::{self, Argument, Code, ConstOps, Function, Instruction, Literal, Program, Type};
+use rand::prelude::*;
 
 // LocationGen is not a typing Context, it is a collection of names such that
 // new or old names can be used when generating new destinations
 // TODO: Do they need to be mapped based on types? or just a
 // set?(Non-deterministically overwrite something of a different type?)
 struct LocationGen {
-    map: HashMap<Type, Vec<String>>,
+    map: HashMap<Type, HashSet<String>>,
     counter: usize,
 }
 
@@ -17,17 +19,16 @@ impl LocationGen {
     pub fn get_location(&mut self, ty: Type) -> String {
         let loc_vec = self.map.entry(ty).or_default();
         if !loc_vec.is_empty() && random() {
-            loc_vec.choose(&mut thread_rng()).unwrap().clone()
+            loc_vec.iter().choose(&mut thread_rng()).unwrap().clone()
         } else {
             let loc = format!("var{}", self.counter);
             self.counter += 1;
-            loc_vec.push(loc.clone());
+            loc_vec.insert(loc.clone());
             loc
         }
     }
 }
 
-// Generates a random integer... you could imagine coming up with a non-uniform distribution
 fn gen_int() -> i64 {
     random()
 }
@@ -64,27 +65,77 @@ fn gen_value_op(location_gen: &mut LocationGen) -> Instruction {
     }
 }
 
-fn gen_function(function_context: (), args: Vec<Argument>, num_blocks: u32) -> Function {
-    todo!();
-    let mut location_gen = LocationGen {
-        map: args.iter().fold(HashMap::new(), |mut acc, arg| {
-            acc.insert(arg.arg_type.clone(), vec![arg.name.clone()]);
-            acc
-        }),
-        counter: 0,
-    };
-    Function {
-        args,
-        instrs: unimplemented!(),
-        name: unimplemented!(),
-        return_type: unimplemented!(),
-    }
+// TODO: Would be interesting to use rand's distributions instead of the
+// standard range
+pub struct Config {
+    pub total_num_functions: NonZeroU32,
+    pub num_blocks_range: Range<u32>,
+    pub num_args_range: Range<u32>,
+    pub rng: StdRng,
 }
 
-pub fn gen_program(num_functions:u32) -> Program {
-    assert!(num_functions > 1, "Must have at least one function");
-    // Iteratively build up functions one after the other
-    Program {
-        functions: unimplemented!(),
+struct FunctionSig {
+    pub name: String,
+    pub args: Vec<Argument>,
+    pub return_type: Option<Type>,
+}
+
+impl Config {
+    fn gen_type(&mut self) -> Type {
+        let binding = [Type::Int, Type::Bool];
+        binding.choose(&mut self.rng).unwrap().clone()
+    }
+
+    fn gen_argument(&mut self, arg_name: String) -> Argument {
+        Argument {
+            name: arg_name,
+            arg_type: self.gen_type(),
+        }
+    }
+
+    fn gen_function(&mut self, sig: &FunctionSig, function_context: Vec<FunctionSig>) -> Function {
+        let typing_context = sig.args.iter().fold(HashMap::new(), |mut acc, arg| {
+            let m: &mut HashSet<_> = acc.entry(arg.arg_type.clone()).or_default();
+            m.insert(arg.name.clone());
+            acc
+        });
+
+        let mut location_gen = LocationGen {
+            map: typing_context.clone(),
+            counter: 0,
+        };
+
+        let blocks = (1..self.rng.gen_range(self.num_args_range.clone())).map(|_| todo!());
+
+        Function {
+            args: sig.args,
+            instrs,
+            name: sig.name,
+            return_type: sig.return_type,
+        }
+    }
+
+    pub fn gen_program(&mut self) -> Program {
+        let mut function_signatures = vec![FunctionSig {
+            name: "main".to_string(),
+            args: vec![],
+            return_type: None,
+        }];
+        let funcs = (1..self.total_num_functions.into()).map(|i| {
+            let args = (0..self.rng.gen_range(self.num_args_range.clone()))
+                .map(|j| self.gen_argument(format!("arg{j}")))
+                .collect();
+            FunctionSig {
+                name: format!("func{i}"),
+                args,
+                return_type: Some(self.gen_type()),
+            }
+        });
+
+        function_signatures.extend(funcs);
+
+        let functions: Vec<Function> = unimplemented!();
+        functions.shuffle(&mut self.rng);
+        Program { functions }
     }
 }
